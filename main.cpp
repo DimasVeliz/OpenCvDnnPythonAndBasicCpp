8f32848bfb6e1d1fa06eb7913bd798d4f38536a2
DimasVeliz <trevor90094@yandex.com>
2021-02-15 21:40:12 +0100

    Fixing problem with previous commit

--- main.cpp
+++ main.cpp
@@ -7,13 +7,16 @@ int main()
     std::string weightFile = "/home/dimas/Desktop/OpenCvDnnPythonAndBasicCpp/weights/yolov3-tiny.weights";
     std::string labels = "//home/dimas/Desktop/OpenCvDnnPythonAndBasicCpp/data/coco.names";
     std::string imagePath = "/home/dimas/Desktop/OpenCvDnnPythonAndBasicCpp/obj/horses.jpg";
+    std::string videoPath = "//home/dimas/Desktop/OpenCvDnnPythonAndBasicCpp/obj/family_crossing.mp4";
 
     DarknetUtility utility(cfgFile,weightFile,labels);
     
     /*TEST*/
     /*std::cout<<utility.sayHi() <<std::endl;*/
     /*utility.capturingFromCamera(0,false);*/
-    utility.doImageProcessing(imagePath);
+    /*utility.doImageProcessing(imagePath);*/
+    /*utility.capturingFromCamera(0,true);*/
+    /*utility.capturingFromCamera(videoPath,true);*/
 
     return 0;
 }
